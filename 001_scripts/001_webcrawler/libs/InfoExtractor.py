400bade4d820cafeb8bbd5c1e0cb2aa50cd557c0
AdrianW424 <adrianwaldera@gmx.de>
2022-11-20 15:21:05 +0100

    adjusted InfoExtractor for UTF8

--- 001_scripts/001_webcrawler/libs/InfoExtractor.py
+++ 001_scripts/001_webcrawler/libs/InfoExtractor.py
@@ -1,16 +1,16 @@
 import re
 
 class InfoExtractor():
-    def getImageAndID(card):
+    def getImageAndID(self, card):
         data = card.find('td', class_='leftCol').find('a').find('img')['src'][5:]
         return "https://gatherer.wizards.com" + data, re.search("multiverseid=(.*)&", data).group(1)
 
-    def getName(card):
+    def getName(self, card):
         return card.find('td', class_='middleCol').find('div', class_='cardInfo').find('span', class_='cardTitle').find('a').contents[0]
 
-    def getType(card):
+    def getType(self, card):
         return re.sub(' +', ' ', re.sub(r'\r\n', "", str(card.find('td', class_='middleCol').find('div', class_='cardInfo').find('span', class_='typeLine').contents[0]))).strip().replace("—", "-")
     
-    def getLink(card):
+    def getLink(self, card):
         # TODO: Link to the website of the card
         pass
