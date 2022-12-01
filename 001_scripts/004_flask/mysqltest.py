7b890dd10f66ec24adb26f2951ad86e2384dd8ca
AdrianW424 <adrianwaldera@gmx.de>
2022-12-01 09:28:44 +0100

    changes

--- 001_scripts/004_flask/mysqltest.py
+++
@@ -1,6 +0,0 @@
-import MySQLHandler as msh
-
-mysqlh = msh.MySQLHandler()
-mysqlh.createMySQLConnNCursor("mysql", 3306, "root", "mysql")
-stmt = mysqlh.stmtHelper("mtg", ["id", "Abandon"], ["name", "Abandon"], ["type", "Abandon"])
-results = mysqlh.requestData(mysqlh.mysqlCursor, stmt)
